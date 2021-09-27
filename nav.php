54edd21afa382d2c8258f2f6b5ed0f78a31c13fa
Smaran Ummadishetty <42758796+smarxn@users.noreply.github.com>
2021-09-27 21:49:23 +0530

    all files added

---
+++ nav.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8" />
+    <title>Rotaract Club of Navi Mumbai</title>
+    <link rel="shortcut icon" href="assets/img/rcnbLogoWhite.png" />
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- css-include -->
+
+    <!-- boorstrap -->
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/bootstrap.min.css"
+    />
+    <!-- themify-icon.css -->
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/themify-icons.css"
+    />
+    <!-- animate.css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css" />
+    <!-- REVOLUTION STYLE SHEETS -->
+    <link rel="stylesheet" type="text/css" href="rev-slider/css/settings.css" />
+    <!-- REVOLUTION LAYERS STYLES -->
+    <link rel="stylesheet" type="text/css" href="rev-slider/css/layers.css" />
+    <!-- REVOLUTION NAVIGATION STYLES -->
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="rev-slider/css/navigation.css"
+    />
+    <!-- menu style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/menu.css" />
+    <!-- style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    <!-- responsive.css -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css" />
+  </head>
+  <!-- /end of head -->
+
+  <body>
+    <!--div id="preloader"></div-->
+    <!-- Start of Header 
+	============================================= -->
+    <header>
+      <div class="header-top-bg">
+        <div class="container">
+          <div class="row">
+            <div class="header-top">
+              <div class="head-top-info pull-left">
+                <ul class="top-info">
+                  <li class="ti-mobile">+91 9619947596</li>
+                  <li class="ti-email">contact@racnavimumbai.org</li>
+                </ul>
+              </div>
+              <!-- /head-top-info -->
+              <div class="header-social pull-right">
+                <div class="social">
+                  <ul class="social-list">
+                    <li>
+                      <a
+                        href="https://www.facebook.com/RotaractClubOfNewBombay/"
+                        ><span class="ti-facebook"></span
+                      ></a>
+                    </li>
+                    <li>
+                      <a href="https://twitter.com/rotaract_new?lang=en"
+                        ><span class="ti-twitter"></span
+                      ></a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.linkedin.com/company/rcnewbombay/?originalSubdomain=in"
+                        ><span class="ti-linkedin"></span
+                      ></a>
+                    </li>
+                    <li>
+                      <a href="https://www.instagram.com/racnavimumbai"
+                        ><span class="ti-instagram"></span
+                      ></a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <!-- /header-social -->
+            </div>
+            <!-- /header-top -->
+          </div>
+          <!-- /row -->
+        </div>
+        <!-- /container -->
+      </div>
+      <div class="menu-bar">
+        <div class="container">
+          <div class="row">
+            <nav class="navbar">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <a class="navbar-brand" href="home-1.html"
+                  ><img src="assets/img/logoo.png" alt="image"
+                /></a>
+              </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div
+                class="collapse navbar-collapse"
+                id="bs-example-navbar-collapse-1"
+              >
+                <ul class="nav navbar-nav navbar-right">
+                  <li><a href="index.html">Home</a></li>
+                  <li>
+                    <a href="cause.html">About</a>
+                    <ul class="menu-dropdown">
+                      <li><a href="cause.html">Who we are</a></li>
+                      <li><a href="cause-single.html">Team 2021-22</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="event.html">Club Projects</a>
+                    <!--ul class="menu-dropdown">
+                      <li><a href="event.html">Events</a></li>
+                      <li><a href="event-single.html">Events Details</a></li>
+                    </ul-->
+                  </li>
+                  <li>
+                    <a href="blog-archive.html">Blog</a>
+                    <!--ul class="menu-dropdown">
+                      <li><a href="blog-archive.html">Blog Archive</a></li>
+                      <li><a href="blog-single.html">Blog Details</a></li>
+                    </ul-->
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                    <!--ul class="menu-dropdown">
+                      <li><a href="contact.html">Contact</a></li>
+                      <li><a href="404.html">404</a></li>
+                      <li><a href="about-us.html">About-us</a></li>
+                    </ul-->
+                  </li>
+                </ul>
+              </div>
+              <!-- /.navbar-collapse -->
+              <div class="home-donate donate-btn-1 text-uppercase">
+                <a href="donation.html">donate now</a>
+              </div>
+              <!--div id="sb-search" class="sb-search">
+                    
+                <form action="#" method="post">
+                     <input
+                    class="sb-search-input"
+                    onkeyup="buttonUp();"
+                    placeholder="Enter Your Search Word..."
+                    type="search"
+                    value=""
+                    name="search"
+                    id="search"
+                  />
+                   <input class="sb-search-submit" type="submit" value="" />
+                   <span class="sb-icon-search"><i class="ti-search"></i></span>
+                      
+                </form>
+              </div-->
+            </nav>
+            <div class="wrap">
+              <!--MENU-->
+              <div id="main-menu">
+                <div class="menu-btn">
+                  <div class="menu-btn-line menu-btn-line-1"></div>
+                  <div class="menu-btn-line menu-btn-line-2"></div>
+                  <div class="menu-btn-line menu-btn-line-3"></div>
+                </div>
+                <div class="moduletable_menu">
+                  <ul class="nav menu">
+                    <li><a href="home-1.html">Home</a></li>
+                    <li><a href="about-us.html">About Us</a></li>
+                    <li><a href="about-us.html">Team 2021-22</a></li>
+                    <li><a href="event.html">Club Projects</a></li>
+                    <li><a href="blog-archive.html">Blog</a></li>
+                    <li><a href="contact.html">Contacts</a></li>
+                    <!--li><a href="blog-single.html">Blog Single</a></li-->
+                    <!--li><a href="cause.html">Club Projects</a></li>
+                    <li><a href="cause-single.html">Cause Details</a></li>
+                    <li><a href="event-single.html">Event Single</a></li>
+                    <li><a href="404.html">404</a></li-->
+                  </ul>
+                </div>
+              </div>
+              <!-- END menu -->
+            </div>
+            <!--/wrap  -->
+          </div>
+          <!--/row  -->
+        </div>
+        <!--/container  -->
+      </div>
+      <!--/menu-bar  -->
+    </header>
