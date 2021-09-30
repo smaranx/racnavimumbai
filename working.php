bba6c970f482ba77f80b427189f4f974bb55afec
Smaran Ummadishetty <42758796+smarxn@users.noreply.github.com>
2021-09-30 14:35:47 +0530

    added under construction page

---
+++ working.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8" />
+    <title>donate</title>
+    <link rel="shortcut icon" href="" />
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- css-include -->
+
+    <!-- boorstrap -->
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="assets/css/bootstrap.min.css"
+    />
+
+  </head>
+  <!-- /end of head -->
+  <body>
+
+    <?php
+     include("nav.php"); 
+    ?>
+
+
+<div class=" ti-align-center ml-auto">
+           <center> <lottie-player
+              src="https://assets7.lottiefiles.com/packages/lf20_jYTS0r.json"
+              speed="1"
+              style="width: 500px; height: 500px"
+              loop
+              autoplay
+            >
+            </lottie-player></center>
+  </div>
+  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+  
+  
+    <?php
+     include("footer.php"); 
+    ?> 
+    <!-- End of footer section
+	============================================= -->
+
+    <!-- js -->
+    <script type="text/javascript" src="assets/js/jquery-2.1.4.min.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
